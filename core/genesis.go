1aa56ca7ed1029ffe4293f5ac9fee847a653fc24
yanhuangpai <yanhuangpai@126.com>
2018-01-30 17:10:06 +0800

    modify blockgaslimit as it was too small

--- core/genesis.go
+++ core/genesis.go
@@ -314,7 +314,7 @@ func DefaultGenesisBlock() *Genesis {
 		Nonce:      66,
 	//	ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
 		ExtraData:  hexutil.MustDecode("0x0000000000000000000000000000000000000000000000000000000000000000"),
-		GasLimit:   4000,
+		GasLimit:   4700000,
 	//	Difficulty: big.NewInt(17179869184),
 		Difficulty: big.NewInt(1048576),
 		Alloc:      decodePrealloc(mainnetAllocData),
